75a7abd32eec205c24ed2783d228607db9e8a3d7
Neo-T <marsstory99@hotmail.com>
2022-03-13 18:16:36 +0800

    coding

--- mmu/buddy.cpp
+++ mmu/buddy.cpp
@@ -19,38 +19,27 @@ static ST_BUDDY_AREA l_staArea[BUDDY_ARER_COUNT];
 static const UINT lr_unPageCount = BUDDY_MEM_SIZE / BUDDY_PAGE_SIZE;
 static ST_BUDDY_PAGE l_staPage[BUDDY_MEM_SIZE / BUDDY_PAGE_SIZE];
 static STCB_BUDDY_PAGE_NODE l_stcbaFreePage[BUDDY_MEM_SIZE / BUDDY_PAGE_SIZE];
-void buddy_init(void)
-{
-	INT i; 
-
-	for (i = 0; i < lr_unPageCount; i++)
-	{
-		l_stcbaFreePage[i].pstPage = &l_staPage[i]; 
-		l_stcbaFreePage[i].pstNext = &l_stcbaFreePage[i + 1];
-	}
-	l_stcbaFreePage[i - 1].pstNext = NULL;
-
-	//* 清零，并把交给buddy管理的整块内存挂载到页块管理数组的最后一个单元，确保最后一个单元为最大一块连续的内存
-	memset(&l_staArea, 0, sizeof(l_staArea));
-	l_staPage[lr_unPageCount - 1].pstNext      = NULL; 
-	l_staPage[lr_unPageCount - 1].pubStart     = l_ubaMemPool; 
-	l_staArea[BUDDY_ARER_COUNT - 1].unPageSize = BUDDY_MEM_SIZE; 	
-	l_staArea[BUDDY_ARER_COUNT - 1].pubStart   = l_ubaMemPool; 
-	l_staArea[BUDDY_ARER_COUNT - 1].pstNext    = &l_staPage[lr_unPageCount - 1]; 
-}
 
 static PST_BUDDY_PAGE GetPageNode(void)
 {
-	PSTCB_BUDDY_PAGE_NODE pstNode = &l_stcbaFreePage[0]; 
+	PST_BUDDY_PAGE pstPage;
+	PSTCB_BUDDY_PAGE_NODE pstNode = &l_stcbaFreePage[0];
 	while (pstNode)
 	{
 		if (pstNode->pstPage)
 		{
-			pstNode->pstPage = NULL; 
-			return pstNode->pstPage; 
+			pstNode->pstPage->blIsUsed = FALSE;
+			pstNode->pstPage->pstNext = NULL;
+			pstPage = pstNode->pstPage;
+			pstNode->pstPage = NULL;
+			return pstPage;
 		}
+
+		pstNode = pstNode->pstNext; 
 	}
-	
+
+	printf("GetPageNode()执行失败\r\n");
+
 	return NULL;
 }
 
@@ -59,22 +48,58 @@ static void FreePageNode(PST_BUDDY_PAGE pstPage)
 	PSTCB_BUDDY_PAGE_NODE pstNode = &l_stcbaFreePage[0];
 	while (pstNode)
 	{
-		if (pstNode->pstPage)
+		if (!pstNode->pstPage)
 		{
-			pstNode->pstPage = NULL;
-			return pstNode->pstPage;
+			pstNode->pstPage = pstPage;
+			return;
 		}
+	}
+
+	printf("FreePageNode()执行失败\r\n");
+}
+
+void buddy_init(void)
+{
+	INT i;
+	UINT unPageSize = BUDDY_PAGE_SIZE;
+
+	//* 存储页面控制信息的链表必须先初始化，接下来就要用到
+	for (i = 0; i < lr_unPageCount; i++)
+	{
+		l_stcbaFreePage[i].pstPage = &l_staPage[i];
+		l_stcbaFreePage[i].pstNext = &l_stcbaFreePage[i + 1];
+	}
+	l_stcbaFreePage[i - 1].pstNext = NULL;
+
+	//* 清零，并把交给buddy管理的整块内存挂载到页块管理数组的最后一个单元，确保最后一个单元为最大一块连续的内存
+	memset(&l_staArea, 0, sizeof(l_staArea));
+	PST_BUDDY_PAGE pstPage = GetPageNode();
+	pstPage->pstNext = NULL;
+	pstPage->pubStart = l_ubaMemPool;
+	l_staArea[BUDDY_ARER_COUNT - 1].pstNext  = pstPage;
+
+	//* 计算并存储各页块管理单元的单个页面大小
+	l_staArea[0].unPageSize = unPageSize;
+	for (i = 1; i < BUDDY_ARER_COUNT; i++)
+	{
+		unPageSize *= 2;
+		l_staArea[i].unPageSize = unPageSize;
 	}
 }
 
 void *buddy_alloc(UINT unSize)
-{		
-	INT i; 
+{
+	INT i;
 	UINT unPageSize = BUDDY_PAGE_SIZE;
-	PST_BUDDY_PAGE pstPage, pstPrevPage, pstNextPage; 
+	PST_BUDDY_PAGE pstPage, pstPrevPage, pstNextPage, pstFreePage;
+	PST_BUDDY_AREA pstArea;
+	PST_BUDDY_PAGE pstFreePage1, pstFreePage2;
 
 	if (unSize > BUDDY_MEM_SIZE)
+	{
+		printf("要申请的内存超过系统支持分配的最大内存\r\n");
 		return (void *)0;
+	}
 
 	//* 确定页块索引
 	for (i = 1; i < BUDDY_ARER_COUNT; i++)
@@ -89,33 +114,35 @@ void *buddy_alloc(UINT unSize)
 	}
 
 	//* 存在有效页面则直接返回
-	pstPage = l_staArea[i].pstNext; 
+	pstPage = l_staArea[i].pstNext;
 	while (pstPage)
 	{
 		if (!pstPage->blIsUsed)
 		{
-			pstPage->blIsUsed = TRUE; 
+			pstPage->blIsUsed = TRUE;
 			return pstPage->pubStart;
 		}
 
-		pstPage = pstPage->pstNext; 
+		pstPage = pstPage->pstNext;
 	}
 
 	//* 一旦执行到这里意味着当前页块单元没有可用页块，需要从更大的页块单元分裂出一块来使用
 	//* 先找到最近的有空余页块的单元
-	i++;	
+	i++;
 	for (; i < BUDDY_ARER_COUNT; i++)
 	{
 		pstPage = l_staArea[i].pstNext;
-		pstPrevPage = NULL; 
+		pstPrevPage = NULL;
 		while (pstPage)
 		{
 			if (!pstPage->blIsUsed)
 				goto __lblSplit;
 			pstPrevPage = pstPage;
-			pstPage = pstPage->pstNext; 			
-		}		
+			pstPage = pstPage->pstNext;
+		}
 	}
+
+	printf("系统无可用内存\r\n");
 
 	return (void *)0;
 
@@ -123,83 +150,114 @@ __lblSplit:
 	for (; i > 0; i--)
 	{
 		//* 摘除
-		if (pstPrevPage)		
+		if (pstPrevPage)
 			pstPrevPage->pstNext = pstPage->pstNext;
 		else
 			l_staArea[i].pstNext = pstPage->pstNext;
 
 		//* 分裂
-		pstNextPage = l_staArea[i - 1].pstNext;
-		while (TRUE)
+		pstArea = &l_staArea[i - 1];
+		pstFreePage1 = GetPageNode();
+		if (!pstFreePage1) //* 这属于程序BUG，理论上不应该申请不到
 		{
-			if (!pstNextPage)
-			{
-				pstNextPage = pstPage->pubStart;
-			}
+			return (void *)0;
 		}
-	}
-
-	//* 执行到这里没有可用页块，需要从更大的页块区分裂出一块来使用
-	i = BUDDY_ARER_COUNT - 1;
-	for (; i >= 0; i--)
-	{		
-		if (unSize > l_staArea[i].unPageSize / 2)
+		pstFreePage2 = GetPageNode();
+		if (!pstFreePage2) //* 同上
 		{
-			if (l_staArea[i].pstNext)
-			{
-				l_staArea[i].pstNext = l_staArea[i].pstNext->pstNext; //* 指向下一个可用节点
-				return (void *)l_staArea[i].pstNext->pubStart;
-			}
-			else
-				break;
+			return (void *)0;
 		}
-		else
+		pstFreePage1->pubStart = pstPage->pubStart;
+		pstFreePage2->pubStart = pstPage->pubStart + pstArea->unPageSize;
+		pstFreePage1->pubBuddyNextAddr = pstFreePage2->pubStart; 
+		pstFreePage1->pubBuddyPrevAddr = NULL; 
+		pstFreePage2->pubBuddyNextAddr = NULL;
+		pstFreePage2->pubBuddyPrevAddr = pstFreePage1->pubStart;
+		FreePageNode(pstPage); //* 归还
+
+		//* 挂接到下一个更小的页块单元
+		pstNextPage = pstArea->pstNext;
+		if (!pstNextPage)
 		{
-			if (l_staArea[i].pstNext)
-			{
-
-			}
-
-		}
-
-		
-
-		if (l_staArea[i].pstNext)
-		{
-			if (unSize > l_staArea[i].unPageSize / 2)
-			{
-				l_staArea[i].pstNext = l_staArea[i].pstNext->pstNext; //* 指向下一个可用节点
-				return (void *)l_staArea[i].pstNext->pubStart;
-			}
+			pstArea->pstNext = pstFreePage1;
+			pstFreePage1->pstNext = pstFreePage2;
+			pstPage = pstFreePage1;
+			pstPrevPage = NULL;
 		}
 		else
 		{
+			do {
+				if (!pstNextPage->pstNext)
+				{
+					pstNextPage->pstNext = pstFreePage1;
+					pstFreePage1->pstNext = pstFreePage2;
+					pstPage = pstFreePage1;
+					pstPrevPage = pstNextPage;
+					break;
+				}
 
+				pstNextPage = pstNextPage->pstNext;
+			} while (TRUE);
 		}
 
-		if (unSize > l_staArea[i].unPageSize / 2)
+		//* 看看当前单元满足分配要求吗，满足则分配，否则继续分裂		
+		if (unSize > pstArea->unPageSize / 2 || BUDDY_PAGE_SIZE == pstArea->unPageSize)
 		{
-			if (l_staArea[i].pstNext)
-				return (void *)l_staArea[i].pstNext->pubStart;
-			else
-				return NULL;
-		}
-
-		//* 如果为空，则需要从前一个内存块分裂出一块给当前内存
-		if (l_staArea[i].pstNext)
-		{
-
-		}
-		else
-		{
-			if()
+			//* 申请容量大于页块容量的一半，不需要继续分裂了，直接返回给用户即可
+			pstPage->blIsUsed = TRUE;
+			return (void *)pstPage->pubStart;
 		}
 	}
 
-	while (TRUE)
-	{
-		
-	}
+	printf("系统无可用内存\r\n");
 
 	return (void *)0;
+}
+
+void buddy_free(void *pvStart)
+{
+	INT i;
+	PST_BUDDY_AREA pstArea;
+	PST_BUDDY_PAGE pstNextPage, pstFreedPage; 
+	UCHAR *pubPage1, *pubPage2; 
+	UCHAR *pubMergedStart; 
+	BOOL blIsNeedToMerge = FALSE; 
+
+	for (i = 0; i < BUDDY_ARER_COUNT; i++)
+	{
+		pstFreedPage = l_staArea[i].pstNext;
+		while (pstFreedPage)
+		{
+			if (pstFreedPage->pubStart == (UCHAR *)pvStart)
+			{
+				if (pstFreedPage->pubBuddyNextAddr)
+				{
+					pubPage1 = pstFreedPage->pubStart;
+					pubPage2 = pstFreedPage->pubBuddyNextAddr;
+				}
+				else
+				{
+					pubPage1 = pstFreedPage->pubBuddyPrevAddr;
+					pubPage2 = pstFreedPage->pubStart;
+				}
+
+				goto __lblFree;
+			}
+
+			pstFreedPage = pstFreedPage->pstNext; 
+		}
+	}
+
+	printf("释放失败\r\n");
+	return;
+
+__lblFree: //* 归还	
+	for (; i < BUDDY_ARER_COUNT; i++)
+	{
+		pstNextPage = l_staArea[i].pstNext;
+		while (pstNextPage)
+		{
+			if(pstNextPage->pubStart == pubPage1)
+		}
+	}
 }
--- mmu/buddy.h
+++ mmu/buddy.h
@@ -17,6 +17,8 @@
 
 typedef struct _ST_BUDDY_PAGE_ { //* 具有相同页面大小的页面链表节点的基本数据结构
     struct _ST_BUDDY_PAGE_ *pstNext;
+	UCHAR *pubBuddyPrevAddr;
+	UCHAR *pubBuddyNextAddr;
 	BOOL blIsUsed; 
     UCHAR *pubStart;     
 } ST_BUDDY_PAGE, *PST_BUDDY_PAGE; 
@@ -29,10 +31,9 @@ typedef struct _STCB_BUDDY_PAGE_NODE_ { //* 页面控制块链表节点
 typedef struct _ST_BUDDY_AREA_ { //* 具有相同页面大小的页块数组单元的基本数据结构
     PST_BUDDY_PAGE pstNext; 
     UINT unPageSize;	//* 该内存块单个页面大小
-	UCHAR *pubStart;	//* 该内存块的第一个页面的起始地址，注意这个算法依赖于内存分配时先从头部开始分配，因为内存释放时依赖这个原则
-    UCHAR ubMap;		//* 标识两个伙伴页面的使用情况：0，两个均空闲或均在使用；1，一个在使用另一个空闲
 } ST_BUDDY_AREA, *PST_BUDDY_AREA; 
 
 BUDDY_EXT void buddy_init(void);		  //* buddy模块初始化函数
 BUDDY_EXT void *buddy_alloc(UINT unSize); //* 只有没有可用的内存块了才会返回NULL，其它情况都会返回一个合适大小的内存块
+BUDDY_EXT void buddy_free(void *pvStart); //* 释放由buddy_alloc分配的内存
 #endif
