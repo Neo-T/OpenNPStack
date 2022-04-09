028fdab8f245fc9f0a496f8f3edb866ed6bb4928
Neo-T <marsstory99@hotmail.com>
2022-04-09 10:02:27 +0800

    Adjusted the typography

--- include/port/datatype.h
+++ include/port/datatype.h
@@ -19,18 +19,18 @@
 #define PACKED_END
 
 //* 系统常用数据类型定义(不同的编译器版本，各数据类型的位宽亦不同，请根据后面注释选择相同位宽的类型定义)
-typedef unsigned long long  ULONGLONG;  //* 64位无符号长整型
-typedef long long           LONGLONG;   //* 64位有符号长整型
-typedef signed long         LONG;       //* 32位的有符号长整型
-typedef unsigned long       ULONG;      //* 32位的无符号长整型
-typedef float               FLOAT;      //* 32位的浮点型
-typedef double              DOUBLE;     //* 64位的双精度浮点型
-typedef signed int          INT;        //* 32位的有符号整型
-typedef unsigned int        UINT;       //* 32位的无符号整型
-typedef signed short        SHORT;      //* 16位的有符号短整型
-typedef unsigned short      USHORT;     //* 16位的无符号短整型
-typedef char                CHAR;       //* 8位有符号字节型
-typedef	unsigned char       UCHAR;      //* 8位无符号字节型
+typedef unsigned long long ULONGLONG;  //* 64位无符号长整型
+typedef long long          LONGLONG;   //* 64位有符号长整型
+typedef signed long        LONG;       //* 32位的有符号长整型
+typedef unsigned long      ULONG;      //* 32位的无符号长整型
+typedef float              FLOAT;      //* 32位的浮点型
+typedef double             DOUBLE;     //* 64位的双精度浮点型
+typedef signed int         INT;        //* 32位的有符号整型
+typedef unsigned int       UINT;       //* 32位的无符号整型
+typedef signed short       SHORT;      //* 16位的有符号短整型
+typedef unsigned short     USHORT;     //* 16位的无符号短整型
+typedef char               CHAR;       //* 8位有符号字节型
+typedef	unsigned char      UCHAR;      //* 8位无符号字节型
 
 #endif
 
