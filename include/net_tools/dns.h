2cc2a2ab81aade27ce3c87e50c125ef725fa1fcb
Neo-T <marsstory99@hotmail.com>
2022-08-24 18:10:34 +0800

    The dns query packet are sent correctly

--- include/net_tools/dns.h
+++ include/net_tools/dns.h
@@ -20,14 +20,14 @@
 //* dns查询报文标志字段
 typedef union _UNI_DNS_FLAG_ {
     struct {
-        USHORT ReplyCode    : 4; //* 响应码，表示响应的差错状态。0，无差错；1,格式错误；2，服务器自身原因无法处理这个请求；3，域名不存在；4，查询类型不支持；5，服务器拒绝；6~15，保留 
-        USHORT resrved      : 3;
-        USHORT RecurAvail   : 1; //* 可用递归，响应报文有效。为 1 时，表示服务器支持递归查询，反之则不支持
-        USHORT RecurDesired : 1; //* 期望递归，查询报文设置，响应报文返回。为1，告诉名称服务器必须处理这个查询得到结果，这被称作递归查；0，且被请求的名称服务器没有一个授权回答，它将返回一个能解答该查询的其他名称服务器列表，这种方式被称为迭代查询
-        USHORT truncated    : 1; //* 表示是否被截断。值为 1 时，表示响应已超过 512 字节并已被截断，只返回前 512 个字节。
-        USHORT auth         : 1; //* 授权应答，响应报文有效。值为 1 时，表示名称服务器是权威服务器；值为 0 时，表示不是权威服务器
-        USHORT opcode       : 4; //* 操作码，查询及响应报文均有效。0，标准查询；1，反向查询；2，服务器状态查询；其它值保留
-        USHORT qr           : 1; //* 查询/响应标识。0，查询；1，响应        
+        USHORT reply_code    : 4; //* 响应码，表示响应的差错状态。0，无差错；1,格式错误；2，服务器自身原因无法处理这个请求；3，域名不存在；4，查询类型不支持；5，服务器拒绝；6~15，保留 
+        USHORT resrved       : 3;
+        USHORT recur_avail   : 1; //* 可用递归，响应报文有效。为 1 时，表示服务器支持递归查询，反之则不支持
+        USHORT recur_desired : 1; //* 期望递归，查询报文设置，响应报文返回。为1，告诉名称服务器必须处理这个查询得到结果，这被称作递归查；0，且被请求的名称服务器没有一个授权回答，它将返回一个能解答该查询的其他名称服务器列表，这种方式被称为迭代查询
+        USHORT truncated     : 1; //* 表示是否被截断。值为 1 时，表示响应已超过 512 字节并已被截断，只返回前 512 个字节。
+        USHORT auth          : 1; //* 授权应答，响应报文有效。值为 1 时，表示名称服务器是权威服务器；值为 0 时，表示不是权威服务器
+        USHORT opcode        : 4; //* 操作码，查询及响应报文均有效。0，标准查询；1，反向查询；2，服务器状态查询；其它值保留
+        USHORT qr            : 1; //* 查询/响应标识。0，查询；1，响应        
     } stb16;
     USHORT usVal;
 } UNI_DNS_FLAG, *PUNI_DNS_FLAG; 
@@ -49,6 +49,6 @@ DNS_EXT INT dns_client_start(in_addr_t *punPrimaryDNS, in_addr_t *punSecondaryDN
 DNS_EXT void dns_client_end(INT nClient); 
 
 //* 实现dns查询，参数pszDomainName指定要查询的域名，返回值为查询结果，如果地址为0，具体错误信息从penErr获得
-DNS_EXT in_addr_t dns_client_query(INT nClient, const CHAR *pszDomainName, EN_ONPSERR *penErr);
+DNS_EXT in_addr_t dns_client_query(INT nClient, in_addr_t unPrimaryDNS, in_addr_t unSecondaryDNS, const CHAR *pszDomainName, EN_ONPSERR *penErr);
 
 #endif
--- net_tools/dns.c
+++ net_tools/dns.c
@@ -8,6 +8,7 @@
 #include "onps_input.h"
 #include "netif/netif.h"
 #include "netif/route.h"
+#include "ip/udp.h"
 #define SYMBOL_GLOBALS
 #include "net_tools/dns.h"
 #undef SYMBOL_GLOBALS
@@ -43,12 +44,11 @@ void dns_client_end(INT nClient)
 //* 封装dns查询类报文
 static UCHAR *dns_queries_encap(const CHAR *pszDomainName, EN_ONPSERR *penErr)
 {
-    //* 申请一块内存，然后根据协议封装必要的字段
-    INT nBytesOf1stSeg;     
+    //* 申请一块内存，然后根据协议封装必要的字段  
     UCHAR *pubDns = buddy_alloc(sizeof(ST_DNS_HDR) + strlen(pszDomainName) + 2 + 4, penErr);
     if (pubDns)
     {
-        UCHAR *pubQueries = pubDns; 
+        UCHAR *pubQueries = pubDns + sizeof(ST_DNS_HDR);
         INT i, k, n = 0, nWriteIdx = 0;
         INT nDomainNameLen = strlen(pszDomainName);
         for (i = 0, k = 1; i < nDomainNameLen; i++, k++)
@@ -84,14 +84,42 @@ static UCHAR *dns_queries_encap(const CHAR *pszDomainName, EN_ONPSERR *penErr)
     return NULL; 
 }
 
-in_addr_t dns_query(INT nClient, const CHAR *pszDomainName, EN_ONPSERR *penErr)
+in_addr_t dns_client_query(INT nClient, in_addr_t unPrimaryDNS, in_addr_t unSecondaryDNS, const CHAR *pszDomainName, EN_ONPSERR *penErr)
 {
-    UCHAR *pubQueryPkt = dns_queries_encap(pszDomainName, penErr); 
-    if (!pubQueryPkt)
-        return; 
+    in_addr_t unDnsSrvIp = inet_addr("192.168.3.210")/*unPrimaryDNS*/;
+    UCHAR *pubDnsQueryPkt = dns_queries_encap(pszDomainName, penErr); 
+    if (!pubDnsQueryPkt)
+        return 0; 
 
-    //* 封装头
+    //* 封装头    
+    PST_DNS_HDR pstHdr = (PST_DNS_HDR)pubDnsQueryPkt; 
+    pstHdr->usTransId = (USHORT)(rand() % (0xFFFF + 1)); 
+    UNI_DNS_FLAG uniFlag; 
+    uniFlag.usVal = 0; 
+    uniFlag.stb16.recur_desired = 1;  //* 递归查询
+    pstHdr->usFlag = htons(uniFlag.usVal); 
+    pstHdr->usQuestions = htons(1); 
+    pstHdr->usAnswerRRs = 0; 
+    pstHdr->usAuthorityRRs = 0; 
+    pstHdr->usAdditionalRRs = 0; 
+
+    //* 发送查询报文，2为单个域名首部长度字节+单个域名尾部结束字节，4为紧跟域名的查询类型与地址类型字段，这两个字段各占两个字节
+    INT nSendBytes = udp_sendto(nClient, unDnsSrvIp, DNS_SRV_PORT, pubDnsQueryPkt, sizeof(ST_DNS_HDR) + strlen(pszDomainName) + 2 + 4);     
+    if (nSendBytes < 0)
+    {
+#if SUPPORT_PRINTF
+    #if PRINTF_THREAD_MUTEX
+        os_thread_mutex_lock(o_hMtxPrintf);
+    #endif
+        printf("udp_sendto() failed, %s\r\n", onps_get_last_error(nClient, NULL));
+    #if PRINTF_THREAD_MUTEX
+        os_thread_mutex_unlock(o_hMtxPrintf);
+    #endif
+#endif
+    }
 
     //* 释放刚才申请的内存
-    buddy_free(pubQueryPkt); 
+    buddy_free(pubDnsQueryPkt); 
+
+    return 0; 
 }
