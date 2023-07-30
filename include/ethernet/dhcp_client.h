4e7afd68e06b5858151d511af30cf9f4807244c4
Neo-T <marsstory99@qq.com>
2023-07-31 00:27:57 +0800

    Complete the writing of the 'raw API' section of the 1.1 version API interface manual.

--- include/ethernet/dhcp_client.h
+++ include/ethernet/dhcp_client.h
@@ -22,7 +22,7 @@ DHCP_CLIENT_EXT INT dhcp_send_packet(INT nInput, PST_NETIF pstNetif, UCHAR ubOpt
 DHCP_CLIENT_EXT void dhcp_renewal_timeout_handler(void *pvParam);
 DHCP_CLIENT_EXT void dhcp_decline(INT nInput, PST_NETIF pstNetif, UINT unTransId, in_addr_t unDeclineIp, in_addr_t unSrvIp);
 DHCP_CLIENT_EXT void dhcp_timer_new(PFUN_ONESHOTTIMEOUT_HANDLER pfunTimeoutHandler, PSTCB_RENEWAL_INFO pstcbRenewalInfo, INT nTimeout);
-DHCP_CLIENT_EXT BOOL dhcp_req_addr(PST_NETIF pstNetif, EN_ONPSERR *penErr);
+DHCP_CLIENT_EXT BOOL dhcp_req_addr(PST_NETIF pstNetif, EN_ONPSERR *penErr); 
 #endif
 
 #endif
